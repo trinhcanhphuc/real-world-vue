da65f717f4884c9bcdd31f12942efd40ef25c04d
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-09-07 16:03:36 +0700

    config vscode

---
+++ src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 v-if @click="handler(arg, event)"></h1>
+</template>
+
+<script>
+import { libName } from 'libName';
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: 
+    },
+  },
+  data() {
+    return {
+      key: value
+    }
+  },
+}
+</script>
+
+<style></style>
